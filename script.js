a2676188194fef3a22a7589a36fa567be858f98f
Ellieanna Ross <74512916+ellieannar@users.noreply.github.com>
2023-12-19 13:47:32 -0800

    Fixing alignment - now dynamic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const logo = document.querySelector(".logo");
-
+    returnFirstBlockHeight();
     window.addEventListener("scroll", function () {
         // Calculate the scale factor based on the scroll position
         const scale = 1 - window.scrollY / (document.documentElement.clientHeight / 2);
@@ -9,3 +9,13 @@ document.addEventListener("DOMContentLoaded", function () {
         logo.style.transform = `scale(${scale > 0 ? scale : 0.01})`;
     });
 });
+
+function returnFirstBlockHeight() {
+    const photosDiv = document.getElementById('first-photos');
+    const image2 = document.getElementById('guitar-playing-image');
+    photosDiv.style.height = (image2.height) + 'px';
+
+    const textDiv = document.getElementById('first-text');
+    const textOne = document.getElementById('first-body')
+    textDiv.style.height = textOne.height + 'px'
+}
